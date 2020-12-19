7d3b0f069b8e17d05614938b9ef4538d3eba20e2
wuttinanhi <wuttinanhi@gmail.com>
2020-12-19 18:09:08 +0700

    Added support for Sendgrid service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const nodemailer = require('nodemailer');
 
+// variable
 const SERVICE = process.env.SERVICE
+
 const SENDER_EMAIL = process.env.SENDER_EMAIL
+const SENDER_USERNAME = process.env.SENDER_USERNAME
+
 const SENDER_PASSWORD = process.env.SENDER_PASSWORD
 
 const RECIEVER_EMAIL  = process.env.RECIEVER_EMAIL
@@ -9,20 +13,38 @@ const SUBJECT  = process.env.SUBJECT
 const CONTENT = process.env.CONTENT
 
 // setup mail transporter service
-const transporter = nodemailer.createTransport({
-  service: SERVICE,
-  auth: {
-    user: SENDER_EMAIL,    // Sender email
-    pass: SENDER_PASSWORD  // Sender password
-  }
-});
+let TransportData = {};
 
-// setup email data with unicode symbols
+// service is sendgrid?
+if(SERVICE === "sendgrid"){
+  TransportData = {
+    host: 'smtp.sendgrid.net',
+    port: 465,
+    secure: true,
+    auth: {
+      user: SENDER_USERNAME,
+      pass: SENDER_PASSWORD
+    }
+  }
+}else{
+  TransportData = {
+    service: SERVICE,
+    auth: {
+      user: SENDER_EMAIL,
+      pass: SENDER_PASSWORD
+    }
+  }
+}
+
+// setup mail transporter service
+const transporter = nodemailer.createTransport(TransportData);
+
+// setup email data
 const mailOptions = {
-  from: SENDER_EMAIL,    // Sender
-  to: RECIEVER_EMAIL,    // Recievers
-  subject: SUBJECT,      // Mail subject
-  html: CONTENT          // HTML body
+  from: SENDER_EMAIL,
+  to: RECIEVER_EMAIL,
+  subject: SUBJECT,
+  html: CONTENT
 };
 
 // send mail with defined transport object
